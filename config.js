16a104e22d521fa477dd1ad9c2bfefdb7991e332
Guy Lunardi <guy.lunardi@collabora.com>
2021-09-18 15:08:29 -0400

    Clean up config.js

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
-//Retrieve room and user details from url
-const url = require('url');
-const current_url = new URL (window.location.href);
-// get access to URLSearchParams object
-const search_params = current_url.searchParams;
-// get url parameters
-const urlroomid = search_params.get('urlroomid');
-const servername = "matrix-testing.lpc.events";
+// Default settings
+const servername = 'matrix-testing.lpc.events';
+const realmname = 'matrix-testing.lpc.events';
 var matchingroomid = '!XtgvTmGxsgQhgfEDuQ:matrix-testing.lpc.events';
+
+// Retrieve room and user details from url
+const current_url = new URL (window.location.href);
+const search_params = current_url.searchParams;
+const urlroomid = search_params.get('urlroomid');
 var urluserid = search_params.get('urluserid');
 var urlemail = search_params.get('urlemail');
-var urlregcode = search_params.get('urlregcode');
+const urlregcode = search_params.get('urlregcode');
 
 // generate roomId from BBB from url parameter
 if (urlroomid){
-    matchingroomid ="!" + urlroomid + ":" + servername;
+    matchingroomid ='!' + urlroomid + ':' + realmname;
+    console.log('Attempting to join room: ' + matchingroomid);
 }
-console.log("Attempting to join room: " + matchingroomid);
 
 if (!urluserid){
-    console.log("No displayname provided!");
-    urluserid = "Unkown user";
+    console.log('No displayname provided!');
+    urluserid = 'Unkown user';
 }
 
 if (urlemail){
-    urlemail = "@" + urlemail.replace("@",".") + ":" + servername ;
-    console.log("Attempting to login as: " + urlemail);
-    console.log("Using reg code: " + urlregcode);
+    urlemail = '@' + urlemail.replace('@','.') + ':' + realmname ;
+    console.log('Attempting to login as: ' + urlemail);
+    console.log('Using reg code: ' + urlregcode);
 }
  
 export let config = {
